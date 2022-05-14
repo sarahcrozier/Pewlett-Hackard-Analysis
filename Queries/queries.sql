1b6e85bcff4b54872a50ba52ceadf614d307c9c6
Sarah Crozier <101441809+sarahcrozier@users.noreply.github.com>
2022-05-13 22:01:14 -0700

    End of Work Day

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -6,3 +6,118 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO dept_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no, first_name, last_name, gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+DROP TABLE emp_info;
+
+-- 7.3.5 List 1: Employee Information
+SELECT e.emp_no, e.first_name, 
+	e.last_name, e.gender, s.salary, 
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	      AND (de.to_date = '9999-01-01');
+		  
+-- 7.3.5 List 2: List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+-- 7.3.5 List 3: Retirees by Department 
+SELECT 	ce.emp_no,
+		ce.first_name,
+		ce.last_name,
+		d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- Create a list for the Sales Department
+SELECT ri.emp_no,
+       ri.first_name,
+	   ri.last_name,
+	   dpt.dept_name
+INTO sales_department_retirees
+FROM dept_info as dpt
+INNER JOIN retirement_info AS ri
+ON (dpt.emp_no = ri.emp_no)
+WHERE dept_name = 'Sales';
+
+-- Create a list of those eligible for for the Mentor Program
+SELECT ri.emp_no,
+       ri.first_name,
+	   ri.last_name,
+	   dpt.dept_name
+INTO Sales_Devl_Retiree
+FROM dept_info as dpt
+INNER JOIN retirement_info AS ri
+ON (dpt.emp_no = ri.emp_no)
+WHERE dept_name in ('Sales', 'Development');
